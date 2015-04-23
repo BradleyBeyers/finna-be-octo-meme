0f3787c8fb77131cf01595ea296a4ef892b0cc8d
Santiago Loane <santiago.loane@rochester.edu>
2015-04-23 00:59:12 -0400

    "Let's build our own engine"
    "That's a way better and easier idea"

--- src/Reversi.java
+++ src/Reversi.java
@@ -6,5 +6,27 @@ public class Reversi {
 		Board board = new Board(8);
 		board.startup();
 		board.render();
+		
+		//default: player one is white
+		boolean player = true;
+		
+	}
+}
+class Turn
+{
+	//true = white, false = black
+	public boolean player;
+	boolean moved;
+	
+	public Turn (boolean Player)
+	{
+		player = Player;
+		moved = false;
+	}
+	public Turn next()
+	{
+		Turn next = new Turn(!player);
+			
+		return next;
 	}
 }
