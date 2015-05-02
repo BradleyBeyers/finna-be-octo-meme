5a94a9cebee7469aa1a90976367fa1a0fd19cfec
BradleyBeyers <beyers.bradley@gmail.com>
2015-05-02 01:19:23 -0400

    forgot the AI's moves are [Y, X] and not [X, Y]

--- finna-be-octo-meme-alex-hankin-santiago-loane-bradley-beyers/Reversi.java
+++ finna-be-octo-meme-alex-hankin-santiago-loane-bradley-beyers/Reversi.java
@@ -1,72 +1,48 @@
+/* Program that drives the AI and handles turn flow control for the game.
+ */
+
 import java.util.*;
 import java.io.*;
-// driver program
+
 public class Reversi {
-	public final static boolean WHITE = true; // Used sometimes for clarity instead of true
-	public final static boolean BLACK = false; // Used sometimes for clarity instead of false
+	public static final boolean WHITE = true; // Used sometimes for clarity instead of true
+	public static final boolean BLACK = false; // Used sometimes for clarity instead of false
 	public static int depthLim;
 	public static int timeLim;
 	public static int totalTimeLim;
-	public static int exploredStates;
-	public static int white = 0;
-	public static int black = 0;
 	public static Board currBoard;
 	public static boolean AiPlayer = false;
 	public static boolean HumanPlayer = false;
 	public static long startTime;
 	public static long endTime;
 	public static long elapsed;
-	
-	//public static Board board = new Board(8);
 
 	public static void main (String[] args) 
 	{
-		System.out.println("Skynet Online.");
 		gameAI();
-		System.out.println("Skynet Offline.");
-		
-		/*for (int i = 0; i < currBoard.pieces.length; i++)
-		{
-			for (int j = 0; j < currBoard.pieces[i].length; j++)
-			{
-				if (currBoard.pieces[i][j] != null && currBoard.pieces[i][j].color == WHITE)
-					white++;
-				else if (currBoard.pieces[i][j] != null && currBoard.pieces[i][j].color == BLACK)
-					black++;
-			}
-		}
-		if (black>white)
-		{
-			System.out.println("White: " + white + "  Black: " + black);
-			System.out.println("Black Wins!! Nice!!");
-		}
-		else if (white>black)
-		{
-			System.out.println("White: " + white + "  Black: " + black);
-			System.out.println("White Wins!! Sweet!!");
-		}
-		else
-		{
-			System.out.println("White: " + white + "  Black: " + black);
-			System.out.println("It's a tie!! Woah!!");
-		}*/
 	}
 
+	// Contains the code for our AI
 	public static void gameAI() {
 		currBoard = new Board(8); //Create a board and declare needed variables for alphabeta
 		currBoard.startup();
 
+		boolean currPlayer = BLACK; // Represents who the current player is (true for white, false for black)
+		boolean gameOver = false;
+		int[] nextMove = null;
+		int[] newMove = null;
+
 		Scanner GameScanner = new Scanner(System.in); //Scanner to parse the initial input that starts the game
 		String init = GameScanner.nextLine();
 		String[] initSplit = init.split(" ");
-		boolean currPlayer = BLACK; // Represents who the current player is (true for white, false for black)
-		int[] nextMove = null;
-		int[] newMove = null;
+
 		depthLim = Integer.valueOf(initSplit[2]);
 		timeLim = Integer.valueOf(initSplit[3]);
 		totalTimeLim = Integer.valueOf(initSplit[4]);
 
-		if  (timeLim != 0) {
+		if (totalTimeLim != 0) {
+			timeLim = totalTimeLim / 64;
+		} else if (timeLim != 0) {
 			depthLim = 0;
 		} else if (depthLim == 0) { // Depth limit defaults to 3 if there is no time limit or depth limit specified
 			depthLim = 3;
@@ -80,18 +56,24 @@ public class Reversi {
 			HumanPlayer = BLACK;
 		}
 
-		boolean cont = true;
-
-		while (cont) { 
+		// Turn flow control as well as the AI handled here
+		while (true) { 
 			currBoard.render();
-			//to prevent turn skipping if the player tries to enter an invalid move
-			if (currPlayer == AiPlayer) {
-				if (!currBoard.MoveDetection(AiPlayer)) {
-					System.out.println("No valid moves for computer player");
+
+			//checks for move availability in both parties, ends game if no available moves
+			if ((!currBoard.MoveDetection(WHITE) && !currBoard.MoveDetection(BLACK)) || currBoard.GameOver()) {
+				gameOver = true;
+				String gameEnd = GameScanner.nextLine();
+				if (gameEnd != null) {
+					System.out.println("pass");
 				}
-				else {
-					exploredStates = 0;
-				
+			}
+
+			// Handles the AI's turn
+			if (currPlayer == AiPlayer && !gameOver) { // If it's the AI's turn...
+				if (!currBoard.MoveDetection(AiPlayer)) { // If the AI has no moves...
+					System.out.println("pass");
+				} else {
 					if (timeLim != 0) { // If we've got a time limit...
 						depthLim = 0; // Reset the depth limit
 						nextMove = null;
@@ -100,106 +82,46 @@ public class Reversi {
 						elapsed = endTime - startTime;
 						while (elapsed + 50 < timeLim) { // Perform iterative deepening with alphabeta to find the move. Cuts off if within 50ms of the time limit
 							nextMove = newMove; // Since alphabeta sometimes returns before the depth limit is reached, we store the current best move here
-							newMove = alphaBeta(currBoard.copy(), 0, -Integer.MAX_VALUE, Integer.MAX_VALUE, AiPlayer)[1];
+							newMove = alphaBeta(currBoard.copy(), 0, -Integer.MAX_VALUE, Integer.MAX_VALUE, AiPlayer)[1]; // Attempt to find a new best move with the new depth limit
 							depthLim++;
 						}
-						System.out.println("Took " + elapsed + "ms to find move and explored " + exploredStates + " states to a depth of " +  depthLim);
 					} else { // Otherwise, we have a depth limit
 						elapsed = -Integer.MAX_VALUE; // Set the elapsed time to the lowest possible value to avoid triggering the time limit cutoff
 						nextMove = alphaBeta(currBoard.copy(), 0, -Integer.MAX_VALUE, Integer.MAX_VALUE, AiPlayer)[1];
 					}
-
-					if(currBoard.MoveDetection(AiPlayer) && nextMove != null) {
-						//if (nextMove != null && nextMove[0] != -1 && nextMove[1] != -1) {
-							currBoard.place(new Piece(nextMove[0], nextMove[1], AiPlayer), true);
-						//}
-					}
-					else
-						System.out.println("No available moves.");
+					currBoard.place(new Piece(nextMove[0], nextMove[1], AiPlayer), true);
+					System.out.println(nextMove[1] + " " +nextMove[0]);
 				}
-			} else if (currPlayer == HumanPlayer) {
+			} else if (currPlayer == HumanPlayer && !gameOver) { // Handles the human's (opponent's) turn
+				boolean validMove = false;
+				while (!validMove) { // If the opponent hasn't given a valid move yet...
+					String humanMove = GameScanner.nextLine(); // Read the opponent's input
+					if (!humanMove.equals("pass")) { // If they didn't pass...
+						String[] humanMoveSplit = humanMove.split(" "); // Parse opponent's move
+						int humanMoveX = Integer.valueOf(humanMoveSplit[0]);
+						int humanMoveY = Integer.valueOf(humanMoveSplit[1]);
 
-				if (!currBoard.MoveDetection(HumanPlayer)) {
-					System.out.println("No valid moves for human player");
-				}
-				else {
-					int humanMoveX = GameScanner.nextInt();
-					int humanMoveY = GameScanner.nextInt();
-
-					if (humanMoveX < 0 || humanMoveX > 7 || humanMoveY < 0 || humanMoveY > 7) {
-						while (humanMoveX < 0 || humanMoveX > 7 || humanMoveY < 0 || humanMoveY > 7) {
-							System.out.println("Not a valid number, Try Again");
-							humanMoveX = GameScanner.nextInt();
-							humanMoveY = GameScanner.nextInt();
-						}
-					}
-					else {
-						boolean validMove = false;
-						//returns true if valid placement
-						if (currBoard.place(new Piece(humanMoveY, humanMoveX, HumanPlayer), true)) {
+						if (humanMoveX < 0 || humanMoveX > 7 || humanMoveY < 0 || humanMoveY > 7) { // Don't let the opponent play out of bounds
 							validMove = false;
-						} else {
-							while (!validMove) {
-								System.out.println("Invalid move, Enter another.");
-								humanMoveX = GameScanner.nextInt();
-								humanMoveY = GameScanner.nextInt();
-								if (currBoard.place(new Piece(humanMoveY, humanMoveX, HumanPlayer), true)) {
-									validMove = true;
-								}
-								else
-									validMove = false;
+						} else { // Check if the move that was given is valid
+							if (currBoard.place(new Piece(humanMoveY, humanMoveX, HumanPlayer), true)) {
+								validMove = true;
 							}
 						}
 					}
 				}
 			}
-
 			currPlayer = !currPlayer;
-
-			//checks for move availability in both parties, ends game if no available moves
-			if (currBoard.MoveDetection(currPlayer) || currBoard.MoveDetection(!currPlayer)) {
-				cont = true;
-			} else if (currBoard.GameOver()) {
-					System.out.println("Game Over. Board is Filled");
-					cont = false;
-			} else {
-				System.out.println("Game Over. No available moves");
-				cont = false;
-
-			}
-		}
-
-		for (int i = 0; i < currBoard.pieces.length; i++) {
-			for (int j = 0; j < currBoard.pieces[i].length; j++) {
-				if (currBoard.pieces[i][j] != null && currBoard.pieces[i][j].color == WHITE)
-					white++;
-				else if (currBoard.pieces[i][j] != null && currBoard.pieces[i][j].color == BLACK)
-					black++;
-			}
-		}
-		
-		if (black>white) {
-			System.out.println("White: " + white + "  Black: " + black);
-			System.out.println("Black Wins!! Nice!!");
-		}
-		else if (white>black) {
-			System.out.println("White: " + white + "  Black: " + black);
-			System.out.println("White Wins!! Sweet!!");
-		}
-		else {
-			System.out.println("White: " + white + "  Black: " + black);
-			System.out.println("It's a tie!! Woah!!");
 		}
 	}
 
 	public static int[][] alphaBeta(Board state, int depth, int alpha, int beta, boolean player) {
-		// state.render();
 		int a = alpha;
 		int b = beta;
 		int value;
 		Board child;
-		int[] bestMove = {-1, -1};
-		int[][] retvalue = {{state.h(AiPlayer)}, bestMove}; // Moves are coded as an integer array of the form [X, Y]
+		int[] bestMove = {-1, -1}; // Moves are coded as an integer array of the form [X, Y]
+		int[][] retvalue = {{state.h(AiPlayer)}, bestMove};
 		ArrayList<int[]> possibleMoves = new ArrayList<int[]>(0);
 
 		if (timeLim != 0) { // If there's a time limit, get the current elapsed time
@@ -209,13 +131,12 @@ public class Reversi {
 
 		if (depth == depthLim || elapsed + 50 > timeLim) return retvalue; // If we're at the depth limit or we're within a 50ms buffer time of the time limit, return
 
-		if (player == AiPlayer) {
+		if (player == AiPlayer) { // The AI is always treated as the max player for this situation
 			possibleMoves = state.getValidMoves(AiPlayer);
-			for (int[] move : possibleMoves) {
-				exploredStates++;
-				child = state.child(move, AiPlayer);
-				value = alphaBeta(child, depth + 1, a, b, !AiPlayer)[0][0];
-				if (value > a) {
+			for (int[] move : possibleMoves) { // For each possible move...
+				child = state.child(move, AiPlayer); // Generate the result of playing that move
+				value = alphaBeta(child, depth + 1, a, b, !AiPlayer)[0][0]; // Recursively find the heuristic value of that move
+				if (value > a) { // Perform alpha-beta pruning
 					a = value;
 					bestMove = move;
 				}
@@ -230,11 +151,10 @@ public class Reversi {
 			return retvalue;
 		} else {
 			possibleMoves = state.getValidMoves(!AiPlayer);
-			for (int[] move : possibleMoves) {
-				exploredStates++;
-				child = state.child(move, !AiPlayer);
-				value = alphaBeta(child, depth + 1, a, b, AiPlayer)[0][0];
-				if (value < b) {
+			for (int[] move : possibleMoves) { // For each possible move...
+				child = state.child(move, !AiPlayer); // Generate the result of playing that move
+				value = alphaBeta(child, depth + 1, a, b, AiPlayer)[0][0]; // Recursively find the heuristic value of that move
+				if (value < b) { // Perform alpha-beta pruning
 					b = value;
 					bestMove = move;
 				}
